0a8039c4743737f746c269ca34a3466cbc2c2861
남성현 <nshn1000@naver.com>
2018-12-31 13:37:14 +0900

    스프링부트 멀티커넥션
    datasource name설정

--- src/main/java/com/hyeon/DatabaseConfig.java
+++ src/main/java/com/hyeon/DatabaseConfig.java
@@ -1,34 +1,44 @@
 package com.hyeon;
-
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 @Configuration
-@MapperScan(basePackages="com.hyeon.dao")
+@MapperScan(basePackages="com.hyeon.dao", sqlSessionFactoryRef="SqlSessionFactory")
 @EnableTransactionManagement
 public class DatabaseConfig {
  
-    @Bean
-    public  SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sessionFactory.setMapperLocations(resolver.getResources("classpath:com/hyeon/dao/*.xml"));
-        return sessionFactory.getObject();
+	@Bean(name = "dataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.db1.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+	
+    @Bean(name = "SqlSessionFactory")
+    @Primary
+    public  SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource, ApplicationContext applicationContext) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:com/hyeon/dao/*.xml"));
+        return sqlSessionFactoryBean.getObject();
     }
     
-    @Bean
+    @Bean(name = "SqlSessionTemplate")
+    @Primary
     public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
-      final SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
-      return sqlSessionTemplate;
+      return new SqlSessionTemplate(sqlSessionFactory);
     }
  
 }
